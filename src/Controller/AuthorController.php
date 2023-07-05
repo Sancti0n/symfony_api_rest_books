1f33d216e2d7c4c120aa72431eaea42966f0e8c6
Jérémie MAUFRET <sayou.jeremie@gmail.com>
2023-07-05 22:55:59 +0200

    Ajout CRUD Book

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuthorRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/api/authors', name: 'author', methods:['GET'])]
+    public function getAuthorList(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse {
+        $authorList = $authorRepository->findAll();
+        $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getAuthors']);
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/authors/{id}', name: 'detailAuthor', methods: ['GET'])]
+    public function getDetailAuthor(int $id, AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse {
+        $author = $authorRepository->find($id);
+        if ($author) {
+            $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
+            return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\Author;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,11 +13,19 @@ class AppFixtures extends Fixture
     {
         // Création d'une vingtaine de livres ayant pour titre
         for ($i = 0; $i < 20; $i++) {
-            $livre = new Book;
+            $author = new Author();
+            $author->setFirstName("Prénom " . $i);
+            $author->setLastName("Nom " . $i);
+            $manager->persist($author);
+            // On sauvegarde l'auteur créé dans un tableau.
+            $listAuthor[] = $author;
+
+            $livre = new Book();
             $livre->setTitle('Livre ' . $i);
             $livre->setCoverText('Quatrième de couverture numéro : ' . $i);
             $livre->setIsbn(strval(random_int(9780000000000, 9790000000000)));
             $manager->persist($livre);
+            $livre->setAuthor($listAuthor[array_rand($listAuthor)]);
         }
         $manager->flush();
     }
